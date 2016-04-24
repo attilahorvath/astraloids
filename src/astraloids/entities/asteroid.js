60b907e6d4e697df2be6de57d2da7ba7f198bec8
Attila Horváth <hun.ati500@gmail.com>
2016-04-24 15:08:34 +0100

    Add triangulation for asteroids

--- src/astraloids/entities/asteroid.js
+++ src/astraloids/entities/asteroid.js
@@ -2,6 +2,8 @@
 
 import Entity from '../entity';
 
+import { triangulate } from '../math';
+
 const mat2 = require('gl-matrix').mat2;
 const mat4 = require('gl-matrix').mat4;
 const vec2 = require('gl-matrix').vec2;
@@ -10,16 +12,34 @@ class Asteroid extends Entity {
   constructor(game, position = vec2.create(), velocity = vec2.create(), acceleration = vec2.create(), angle = 0.0, angularVelocity = 0.0, angularAcceleration = 0.0) {
     super(game, position, velocity, acceleration, angle, angularVelocity, angularAcceleration);
 
-    this.vertices = [];
-
-    let color = 0.2 + Math.random() * 0.5;
+    this.points = [];
 
     for (let vertexAngle = 0.0; vertexAngle < Math.PI * 2.0; vertexAngle += Math.PI / (3.0 + Math.random() * 6.0)) {
       let vertexPosition = vec2.fromValues(0.0, -10.0 - Math.random() * 50.0);
       let rotation = mat2.create();
       mat2.rotate(rotation, rotation, vertexAngle);
       vec2.transformMat2(vertexPosition, vertexPosition, rotation);
-      this.vertices.push(vertexPosition[0], vertexPosition[1], 0.0, 0.0, 1.0, 0.5, 1.0);
+      this.points.push(vertexPosition);
+    }
+
+    this.trianglePoints = triangulate(this.points);
+
+    this.triangleVertices = [];
+
+    for (let point of this.trianglePoints) {
+      this.triangleVertices.push(point[0], point[1], 0.0, 1.0, 1.0, 1.0, 1.0);
+    }
+
+    this.triangleVertexBuffer = game.renderer.createVertexBuffer(this.triangleVertices);
+
+    this.simpleShader = game.renderer.shaders.simpleShader;
+
+    this.vertices = [];
+
+    let color = 0.2 + Math.random() * 0.5;
+
+    for (let point of this.points) {
+      this.vertices.push(point[0], point[1], 0.0, 0.0, 1.0, 0.5, 1.0);
     }
 
     this.vertexBuffer = game.renderer.createVertexBuffer(this.vertices);
@@ -30,6 +50,8 @@ class Asteroid extends Entity {
   }
 
   draw(renderer, deltaTime, transformation = mat4.create()) {
+    renderer.draw(this.simpleShader, transformation, this.triangleVertexBuffer, renderer.gl.TRIANGLES, this.triangleVertices.length / 7);
+
     this.pointShader.pointSizeValue = 8.0;
     renderer.draw(this.pointShader, transformation, this.vertexBuffer, renderer.gl.POINTS, this.vertices.length / 7);
     renderer.draw(this.pointShader, transformation, this.vertexBuffer, renderer.gl.LINE_LOOP, this.vertices.length / 7);
--- src/astraloids/math.js
+++ src/astraloids/math.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const vec2 = require('gl-matrix').vec2;
+const vec3 = require('gl-matrix').vec3;
 
 function pointInTriangle(point, a, b, c) {
   let v0 = vec2.subtract(vec2.create(), c, a);
@@ -20,4 +21,64 @@ function pointInTriangle(point, a, b, c) {
   return u >= 0.0 && v >= 0.0 && u + v < 1.0;
 }
 
-export { pointInTriangle };
+function findEar(vertices) {
+  if (vertices.length <= 3) {
+    return [0, 1, 2];
+  }
+
+  for (let index = 0; index < vertices.length; index++) {
+    let previousIndex = (vertices.length + index - 1) % vertices.length;
+    let nextIndex = (index + 1) % vertices.length;
+
+    let previous = vertices[previousIndex];
+    let current = vertices[index];
+    let next = vertices[nextIndex];
+
+    let toCurrent = vec2.subtract(vec2.create(), current, previous);
+    let toNext = vec2.subtract(vec2.create(), next, previous);
+    let cross = vec2.cross(vec3.create(), toCurrent, toNext);
+
+    if (cross[2] > 0) {
+      let ear = true;
+
+      for (let i = 0; i < vertices.length; i++) {
+        if (i !== previousIndex && i !== index && i !== nextIndex && pointInTriangle(vertices[i], previous, current, next)) {
+          ear = false;
+          break;
+        }
+      }
+
+      if (ear) {
+        return [previousIndex, index, nextIndex];
+      }
+    }
+  }
+
+  return null;
+}
+
+function triangulate(vertices) {
+  let remainingVertices = vertices.slice();
+
+  let triangles = [];
+
+  while (remainingVertices.length > 2) {
+    let ear = findEar(remainingVertices);
+
+    if (ear) {
+      triangles.push(remainingVertices[ear[0]], remainingVertices[ear[1]], remainingVertices[ear[2]]);
+
+      remainingVertices.splice(ear[1], 1);
+    } else {
+      return null;
+    }
+  }
+
+  //let triangleIndices = triangles.map(triangle => vertices.indexOf(triangle));
+
+  //return triangleIndices;
+
+  return triangles;
+}
+
+export { pointInTriangle, findEar, triangulate };
